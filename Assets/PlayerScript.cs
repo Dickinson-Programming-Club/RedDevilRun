f161f3809920d40140bf0bf2af0f184e66b61dd4
wpgoble <will.goble@gmail.com>
2025-04-01 20:59:47 -0400

    encapsulates player movement

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem;
 public class PlayerScript : MonoBehaviour
 {
     public float speed = 5f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,14 +12,19 @@ public class PlayerScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        MovePlayer();
+    }
+
+    void MovePlayer() {
+        float movement = speed * Time.deltaTime;
+
         if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
-            transform.position += new Vector3(speed * Time.deltaTime, 0f, 0f);
+            transform.position += new Vector3(movement, 0f, 0f);
         } else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
-            transform.position -= new Vector3(speed * Time.deltaTime, 0f, 0f);
+            transform.position -= new Vector3(movement, 0f, 0f);
         } else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
-            transform.position += new Vector3(0f, speed * Time.deltaTime, 0f);
+            transform.position += new Vector3(0f, movement, 0f);
         }
     }
 }
