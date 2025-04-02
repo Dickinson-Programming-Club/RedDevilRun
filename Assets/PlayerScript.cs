6ed3d3d0fe46bad94c90e116836986171c9d7519
wpgoble <will.goble@gmail.com>
2025-04-01 20:47:25 -0400

    adds arrow key functionality to movement

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
@@ -12,11 +13,11 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.D)) {
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
             transform.position += new Vector3(speed * Time.deltaTime, 0f, 0f);
-        } else if (Input.GetKey(KeyCode.A)) {
+        } else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
             transform.position -= new Vector3(speed * Time.deltaTime, 0f, 0f);
-        } else if (Input.GetKey(KeyCode.W)) {
+        } else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
             transform.position += new Vector3(0f, speed * Time.deltaTime, 0f);
         }
     }
